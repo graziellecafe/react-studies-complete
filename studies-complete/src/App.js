0055ba2e57d804de3172f34ab696a75d364f3d53
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-08-27 19:09:49 -0300

    feat: conhecendo sobre tags

--- studies-complete/src/App.js
+++ studies-complete/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nome = "Grazielle"; 
+
   return (
     <div className="App">
       <h1>Olá mundo</h1>
       <p>Meu primeiro app</p>
+      <p>Entendendo tags</p>
     </div>
   );
 }
