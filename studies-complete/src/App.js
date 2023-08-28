dca9d486972e971410e7e5f7d4ceddf6badb65f1
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-08-28 12:30:37 -0300

    feat: jsx e imagens no react

--- studies-complete/src/App.js
+++ studies-complete/src/App.js
@@ -5,11 +5,22 @@ function App() {
 
   const nome = "Grazielle"; 
 
+  function sum (a,b) {
+    return a + b 
+
+  }
+
+  const url ='https://via.placeholder.com/150'; 
+
   return (
     <div className="App">
       <h1>Olá mundo</h1>
       <p>Meu primeiro app</p>
       <p>Entendendo tags</p>
+      <p>Meu nome é {nome} </p>
+      <p>Soma: { 2 + 2 } </p>
+      <p>Soma: { sum (1,2)} </p>
+      <img src={url} alt="Google" />
     </div>
   );
 }
